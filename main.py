3e27141b3c76e20d69844aeb89045183f0f4a0f7
Ben Vincent <ben@unkin.net>
2022-05-16 19:57:32 +0930

    Updated mytoolsapi to take advantage of memcached

--- main.py
+++ main.py
@@ -2,24 +2,43 @@ from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl, ValidationError
 from requests_html import HTMLSession, AsyncHTMLSession
 from bs4 import BeautifulSoup
+from pymemcache.client import base
 import uvicorn
+from base64 import b64encode, b64decode
 
+# create connection to local memcached service
+cache = base.Client(('localhost', 11211))
+
+# create request item class
 class urlrender_request(BaseModel):
     url: HttpUrl
     sleep: int
 
+# start the fastapi app
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to MyToolsAPI"}
 
 @app.post("/url/render/title")
 async def url_render_title(item: urlrender_request):
-    session = AsyncHTMLSession()
-    resp = await session.get(item.url)
-    await resp.html.arender(sleep=item.sleep)
-    soup = BeautifulSoup(resp.html.html, "lxml")
+    # create a base64 encode of the URL as a key for memcached
+    urlencode: str = b64encode(item.url.encode('ascii'))
+    # check memcached for the key
+    if cache.get(urlencode) is None:
+        # doesnt exist, so render the page normally
+        session = AsyncHTMLSession()
+        resp = await session.get(item.url)
+        await resp.html.arender(sleep=item.sleep)
+        soup = BeautifulSoup(resp.html.html, "lxml")
+        # encode soup object as base64, then store in memcached
+        soupcode: str = b64encode(soup.encode('ascii'))
+        cache.set(urlencode, soupcode)
+    else:
+        # already exists in memcached, create a new soup object from memcached
+        soup = BeautifulSoup(b64decode(cache.get('test')).decode("utf-8", "ignore"), 'lxml')
+    # return the data as json
     return {
         "url": f"{item.url}",
         "title": f"{soup.title}"
@@ -27,10 +46,22 @@ async def url_render_title(item: urlrender_request):
 
 @app.post("/url/render/body")
 async def url_render_body(item: urlrender_request):
-    session = AsyncHTMLSession()
-    resp = await session.get(item.url)
-    await resp.html.arender(sleep=item.sleep)
-    soup = BeautifulSoup(resp.html.html, "lxml")
+    # create a base64 encode of the URL as a key for memcached
+    urlencode: str = b64encode(item.url.encode('ascii'))
+    # check memcached for the key
+    if cache.get(urlencode) is None:
+        # doesnt exist, so render the page normally
+        session = AsyncHTMLSession()
+        resp = await session.get(item.url)
+        await resp.html.arender(sleep=item.sleep)
+        soup = BeautifulSoup(resp.html.html, "lxml")
+        # encode soup object as base64, then store in memcached
+        soupcode: str = b64encode(soup.encode('ascii'))
+        cache.set(urlencode, soupcode)
+    else:
+        # already exists in memcached, create a new soup object from memcached
+        soup = BeautifulSoup(base64.b64decode(cache.get('test')).decode("utf-8", "ignore"), 'lxml')
+    # return the data as json
     return {
         "url": f"{item.url}",
         "body": f"{soup.body}"
